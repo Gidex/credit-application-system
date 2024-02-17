6dfd20e9e21a6baf937d68a744a970921cd48d52
Gidex <32272099+Gidex@users.noreply.github.com>
2024-02-17 17:43:25 -0300

    implement credit service

--- src/main/kotlin/dev/gidex/cas/service/ICreditService.kt
+++ src/main/kotlin/dev/gidex/cas/service/ICreditService.kt
@@ -1,4 +1,12 @@
 package dev.gidex.cas.service
 
-class ICreditService {
+import dev.gidex.cas.entity.Credit
+import java.util.UUID
+
+interface ICreditService {
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomerId(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
 }
--- src/main/kotlin/dev/gidex/cas/service/impl/CreditService.kt
+++ src/main/kotlin/dev/gidex/cas/service/impl/CreditService.kt
@@ -1,4 +1,29 @@
 package dev.gidex.cas.service.impl
 
-class CreditService {
+import dev.gidex.cas.entity.Credit
+import dev.gidex.cas.repository.CreditRepository
+import dev.gidex.cas.service.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+): ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+        return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomerId(customerId: Long): List<Credit> =
+        this.creditRepository.findAllByCustomerId(customerId)
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit = this.creditRepository.findByCreditCode(creditCode) ?: throw  RuntimeException("Credit code $creditCode not found")
+
+        return if (credit.customer?.id == customerId) credit else throw RuntimeException("Contact admin")
+    }
 }
